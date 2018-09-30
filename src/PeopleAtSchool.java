ca58940adc2225c9d9b1ab9c0509c72006db7b7c
Basia Kmiecik <basiakmiecik@gmail.com>
2018-09-30 13:12:28 +0200

    dodanie klasy nadrzednej PoepleAtScool

---
+++ src/PeopleAtSchool.java
@@ -0,0 +1,25 @@
+public class PeopleAtSchool {
+    String firstName;
+    String lastNem;
+
+    public PeopleAtSchool(String firstName, String lastNem) {
+        this.firstName = firstName;
+        this.lastNem = lastNem;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastNem() {
+        return lastNem;
+    }
+
+    public void setLastNem(String lastNem) {
+        this.lastNem = lastNem;
+    }
+}

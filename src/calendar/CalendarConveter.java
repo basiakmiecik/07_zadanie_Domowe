e43612000ca188d848dde99808287f3909fe3f46
Basia Kmiecik <basiakmiecik@gmail.com>
2018-10-04 21:36:41 +0200

    poprawienie zadania 1

--- src/calendar/CalendarConveter.java
+++ src/calendar/CalendarConveter.java
@@ -1,48 +1,49 @@
 package calendar;
 
 public class CalendarConveter {
-    int monday=1;
-    int tuesday=2;
-    int wendsday=3;
-    int thursday=4;
-    int friday=5;
-    int saturday=6;
-    int sunday=7;
+    private static final int monday = 1;
+    private static final int tuesday = 2;
+    private static final int wednesday = 3;
+    private static final int thursday = 4;
+    private static final int friday = 5;
+    private static final int saturday = 6;
+    private static final int sunday = 7;
 
 
-    public static String convertDayToString(int dayNumber){
-        String day=null;
-        switch(dayNumber){
-            case 1:{
-                day="Poniedziałek";
+    public static String convertDayToString(int dayNumber) {
+        String day = null;
+        switch (dayNumber) {
+            case monday: {
+                day = "Poniedziałek";
                 break;
             }
-            case 2:{
-                day="Wtorek";
+            case tuesday: {
+                day = "Wtorek";
                 break;
             }
-            case 3:{
-                day="Środa";
+            case wednesday: {
+                day = "Środa";
                 break;
             }
-            case 4:{
-                day="Czwartek";
+            case thursday: {
+                day = "Czwartek";
                 break;
             }
-            case 5:{
-                day="Piątek";
+            case friday: {
+                day = "Piątek";
                 break;
             }
-            case 6:{
-                day="Sobota";
+            case saturday: {
+                day = "Sobota";
                 break;
             }
-            case 7:{
-                day="Niediela";
+            case sunday: {
+                day = "Niediela";
                 break;
             }
         }
 
-        return day; }
+        return day;
+    }
 
 }
